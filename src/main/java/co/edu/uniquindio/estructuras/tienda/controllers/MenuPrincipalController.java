34408eb2ca0cde6c0ce4d7e02481640af50907e6
Juan Manuel Amador Roa <arroa03@gmail.com>
2024-02-26 17:29:52 -0500

    Create the main menu fxml and controller

---
+++ src/main/java/co/edu/uniquindio/estructuras/tienda/controllers/MenuPrincipalController.java
@@ -0,0 +1,42 @@
+package co.edu.uniquindio.estructuras.tienda.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.shape.SVGPath;
+
+public class MenuPrincipalController {
+
+	@FXML
+	private SVGPath svgShoppingCard;
+
+	@FXML
+	void buscarEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void helpEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void productsEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void profileEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void shoppingCardEvent(ActionEvent event) {
+
+	}
+
+	@FXML
+	void ventasEvent(ActionEvent event) {
+
+	}
+
+}
