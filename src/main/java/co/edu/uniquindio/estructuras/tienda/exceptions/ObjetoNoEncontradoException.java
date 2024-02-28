c64d0baeb74b08b43aad5021b73ad66cf1301b80
Tourment0412 <94409923+Tourment0412@users.noreply.github.com>
2024-02-28 13:21:20 -0500

    Added some methods to Tienda and some settings for them

---
+++ src/main/java/co/edu/uniquindio/estructuras/tienda/exceptions/ObjetoNoEncontradoException.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.estructuras.tienda.exceptions;
+
+public class ObjetoNoEncontradoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ObjetoNoEncontradoException(String msj) {
+		super(msj);
+		
+	}
+	
+	
+
+}
